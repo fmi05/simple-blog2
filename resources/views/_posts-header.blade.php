865a9a38f5cbafb578430aa5464313159a16f525
fmi05 <112628973+fmi05@users.noreply.github.com>
2023-05-05 09:41:26 +0000

    Stand 2023-05-05

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -14,12 +14,24 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+            <div x-data="{ show: false }">
+                <button @click="show = ! show" class="py-2 pl-3 pr-9 text-sm font-semibold">Categories</button>
+                <div x-show="show">
+                    <a href="#">One</a>
+                    <a href="#">Two</a>
+                    <a href="#">Three</a>
+                </div>
+            </div>
+            <!--
             <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
                 <option value="category" disabled selected>Category
                 </option>
-                <option value="personal">Personal</option>
-                <option value="business">Business</option>
+                @foreach ($category as $category)
+                <option value="{{ $category->slug }}">{{ $category->name }}</option>
+                @endforeach
             </select>
+            -->
+            
 
             <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                     height="22" viewBox="0 0 22 22">
