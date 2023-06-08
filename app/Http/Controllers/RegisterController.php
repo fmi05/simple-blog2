d0ef413c8d1acb3f533b9e9f1c5cfe67a41882e7
fmi05 <fabio.mendez.inclan@urs-os.de>
2023-06-08 17:36:32 +0200

    Complete Episode 45

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create(){
+        return view('register.create');
+    }
+
+    public function store(){
+        //create the user
+        User::create(request()->validate([
+            'name' => ['required', 'max:255'],
+            'username' => ['required', 'min:3', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'min:7', 'max:255'],
+        ]));
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
