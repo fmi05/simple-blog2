77d1c34b487fc2368c232bce825accbdce23ce6a
fmi05 <fabio.mendez.inclan@urs-os.de>
2023-06-11 17:51:23 +0200

    Complete Episode 50

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationExeption;
+
+
 class SessionsController extends Controller
 {
+    public function create(){
+        return view('sessions.create');
+    }
+
+    public function store(){
+        //validate the request
+        $attributes=request()->validate([
+            'email'=>'requiered|exists:unique:users,email',
+            'password'=>'requiered'
+        ]);
+
+        //attempt authenticate and to log in the user
+        //based on the provided credentials
+        if(auth()->attempt($attributes)){
+            session()-regenerate();
+            
+            //redirect wirh a success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        //auth failed
+        throw ValidationExeption::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+    }
+
     public function destroy(){
         auth()->logout();
 
